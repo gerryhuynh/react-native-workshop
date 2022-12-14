10a3af18f42f2a58b080fe8db105f8506a0bc076
Gerry Huynh <gerry.huynh@shopify.com>
2022-12-14 00:37:38 -0500

    Add Schedule Screen

--- conference-app/App.js
+++ conference-app/App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
+import ScheduleScreen from './screens/ScheduleScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <DetailsScreen />
+      <ScheduleScreen />
     </View>
   );
 };
